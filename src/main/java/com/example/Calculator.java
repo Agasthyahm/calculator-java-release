17c62cc1b1ab67077a0c2341101012001fcfa1ce
Agasthyahm <hmagasthya@gmail.com>
2026-01-07 23:08:12 +0530

    Add main method to Calculator class

--- src/main/java/com/example/Calculator.java
+++ src/main/java/com/example/Calculator.java
@@ -1,7 +1,11 @@
 package com.example;
 
 public class Calculator {
-    
+
+    public static void main(String[] args) {
+        System.out.println("Calculator app started");
+    }
+
     public int add(int a, int b) {
         return a + b;
     }
